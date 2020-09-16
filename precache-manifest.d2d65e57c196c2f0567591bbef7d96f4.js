11d9e3805400a905673a72cc21fb2a139ef70d28
Guy Black <guyb2602@gmail.com>
2020-09-15 21:22:45 -0500

    first build up

---
+++ precache-manifest.d2d65e57c196c2f0567591bbef7d96f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27bef2f2ae54af13bd22d3770016fc8",
+    "url": "/brite-pegs/index.html"
+  },
+  {
+    "revision": "eb13255e9127f16aca26",
+    "url": "/brite-pegs/static/css/main.27b85264.chunk.css"
+  },
+  {
+    "revision": "de718a88bb76293751a8",
+    "url": "/brite-pegs/static/js/2.28815cc5.chunk.js"
+  },
+  {
+    "revision": "581070db7478fba8a52f7e9e4c335e11",
+    "url": "/brite-pegs/static/js/2.28815cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb13255e9127f16aca26",
+    "url": "/brite-pegs/static/js/main.31369ea5.chunk.js"
+  },
+  {
+    "revision": "1af3442235d7783f3cab",
+    "url": "/brite-pegs/static/js/runtime-main.c109dafa.js"
+  }
+]);
